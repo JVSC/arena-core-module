82e0b490dd3d35c7661bfdc3917fa9e8acadabae
JVSC <joaovictor.clementino@gmail.com>
2023-03-18 17:31:01 -0300

    feat: implemenet skills's constants and interfaces

---
+++ src/skills/skills.interfaces.ts
@@ -0,0 +1,22 @@
+import { PersistenceEnum, TypingEnum } from '../common/constants';
+import { SkillClass } from './skills.constants';
+
+export interface SkillInterface {
+  characterId: number;
+  description: string;
+  name: string;
+  class: SkillClass;
+  persistence: PersistenceEnum;
+  cooldown: number;
+  baseCooldown: number;
+  picture: string;
+  pictureBanner: string;
+  typing: TypingEnum;
+}
+
+export interface IngameSkillInterface extends SkillInterface {
+  activeCooldown: number;
+  characterGameId: number;
+  playerId: number;
+  isUsable: boolean;
+}
