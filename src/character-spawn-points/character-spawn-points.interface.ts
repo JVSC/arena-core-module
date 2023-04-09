3d5ca77d3f6fa288a0e67a33fb1cb9377a8bf748
JVSC <joaovictor.clementino@gmail.com>
2023-04-09 16:31:05 -0300

    feat: add new module for character spawn points

---
+++ src/character-spawn-points/character-spawn-points.interface.ts
@@ -0,0 +1,7 @@
+export interface CharacterSpawnPointsInterface {
+  id: number;
+  cellIndex: number;
+  spawnRate: number;
+  overworldId: number;
+  characterId: number;
+}
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './effects';
 export * from './common';
 export * from './skills';
 export * from './overworlds';
+export * from './character-spawn-points';
