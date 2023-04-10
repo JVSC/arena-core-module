4c2d1242a73a0a192f891234701a9c9d1aac1cfd
JVSC <joaovictor.clementino@gmail.com>
2023-04-10 01:53:19 -0300

    feat: add new 'complete overworld' interface

--- src/overworlds/overworlds.interface.ts
+++ src/overworlds/overworlds.interface.ts
@@ -1,6 +1,15 @@
+import { CharacterInterface } from '../characters';
+import { CharacterSpawnPointsInterface } from '../character-spawn-points';
+
+interface SpawnPoint extends CharacterSpawnPointsInterface {
+  character?: CharacterInterface;
+}
 export interface OverworldInterface {
   id: number;
   name: string;
   description: string;
   mapPicture: string;
+}
+export interface CompleteOverworldInterface extends OverworldInterface {
+  spawns?: [SpawnPoint];
 }
